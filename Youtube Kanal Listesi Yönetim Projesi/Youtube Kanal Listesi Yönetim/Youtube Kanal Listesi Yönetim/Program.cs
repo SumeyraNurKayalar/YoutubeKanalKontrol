433c0217eb837cd6f8d965cc15968d5118cfa6ca
Sümeyra Nur Kayalar <149170501+SumeyraNurKayalar@users.noreply.github.com>
2025-08-13 13:19:32 +0300

    Update Program.cs

--- Youtube Kanal Listesi Yönetim Projesi/Youtube Kanal Listesi Yönetim/Youtube Kanal Listesi Yönetim/Program.cs
+++ Youtube Kanal Listesi Yönetim Projesi/Youtube Kanal Listesi Yönetim/Youtube Kanal Listesi Yönetim/Program.cs
@@ -1,21 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 using OfficeOpenXml;
+using System.Reflection;
+using Youtube_Kanal_Listesi_Yönetim.Data;
+using Youtube_Kanal_Listesi_Yönetim.Services;
 
-ExcelPackage.LicenseContext = LicenseContext.NonCommercial; var builder = WebApplication.CreateBuilder(args);
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-builder.Services.AddControllers();
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IAppDbContext>(provider =>
+    new AppDbContext(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IChannelService, ChannelService>();
+builder.Services.AddScoped<CsService, CsService>();
+builder.Services.AddScoped<ExcelService, ExcelService>();
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "YouTube Kanal Yönetim API",
+        Version = "v1",
+        Description = "YouTube kanal bilgilerini yönetmek için API"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    if (File.Exists(xmlPath))
+    {
+        c.IncludeXmlComments(xmlPath);
+    }
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "YouTube Kanal API v1");
+
+});
+
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
