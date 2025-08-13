5bbb36da7741a96b63488580dbd9244a4db66406
Sümeyra Nur Kayalar <149170501+SumeyraNurKayalar@users.noreply.github.com>
2025-08-13 13:16:56 +0300

    Update IChannelService.cs

--- Youtube Kanal Listesi Yönetim Projesi/Youtube Kanal Listesi Yönetim/Youtube Kanal Listesi Yönetim/Services/IChannelService.cs
+++ Youtube Kanal Listesi Yönetim Projesi/Youtube Kanal Listesi Yönetim/Youtube Kanal Listesi Yönetim/Services/IChannelService.cs
@@ -5,6 +5,8 @@ namespace Youtube_Kanal_Listesi_Yönetim.Models
 {
     public interface IChannelService
     {
+        List<Channel> GetAllChannels();
+        void AddChannel(Channel channel);
         Task<List<Channel>> GetAllChannelsAsync();
         Task<List<Channel>> SearchChannelsAsync(string searchTerm);
         Task<List<Channel>> FilterChannelsByCategoryAsync(string category);
