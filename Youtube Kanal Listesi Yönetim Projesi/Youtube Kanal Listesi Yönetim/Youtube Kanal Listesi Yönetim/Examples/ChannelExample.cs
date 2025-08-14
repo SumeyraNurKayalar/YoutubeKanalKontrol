ffd291a3d2bae63947fd34cbe4f31d7d5ae6b36e
Sümeyra Nur Kayalar <149170501+SumeyraNurKayalar@users.noreply.github.com>
2025-08-14 10:18:12 +0300

    Add files via upload

---
+++ Youtube Kanal Listesi Yönetim Projesi/Youtube Kanal Listesi Yönetim/Youtube Kanal Listesi Yönetim/Examples/ChannelExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Swashbuckle.AspNetCore.Filters;
+using Youtube_Kanal_Listesi_Yönetim.Models;
+
+namespace Youtube_Kanal_Listesi_Yönetim.Examples
+{
+    public class ChannelListExample : IExamplesProvider<List<Channel>>
+    {
+        public List<Channel> GetExamples()
+        {
+            return new List<Channel>
+            {
+                new Channel
+                {
+                    IsActive = true,
+                    ChannelId = "UC999999",
+                    ChannelName = "Deneme 1",
+                    Category = "Yemek",
+                    Subscribers = "5000",
+                    CreatedAt = DateTime.UtcNow
+                }
+            };
+        }
+    }
+}
